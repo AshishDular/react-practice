96ad2c1877c6ed1a20919cc364a01838cc56613c
Ashish Dular <119157107+AshishDular@users.noreply.github.com>
2023-05-01 17:42:05 +0530

    react practice project

---
+++ src/Books.js
@@ -0,0 +1,66 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.webp";
+import img4 from "./images/book4.webp";
+import img5 from "./images/book5.jpg";
+import img6 from "./images/book6.webp";
+import img7 from "./images/book7.webp";
+import img8 from "./images/book8.webp";
+import img9 from "./images/book9.jpg";
+
+export const books = [
+  {
+    author: "Ramit Sethi",
+    title: "I Will Teach You to Be Rich",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "The Ballad of Songbirds and Snakes",
+    img: img3,
+    id: 3,
+  },
+  {
+    author: "Emily Henry",
+    title: "Happy Place",
+    img: img4,
+    id: 4,
+  },
+  {
+    author: "Bonnie Garmus",
+    title: "Lessons in Chemistry",
+    img: img5,
+    id: 5,
+  },
+  {
+    author: "Robert Greene",
+    title: "The 48 Laws of Power",
+    img: img6,
+    id: 6,
+  },
+  {
+    author: "Laura Dave",
+    title: "The Last Thing He Told Me: A Novel",
+    img: img7,
+    id: 7,
+  },
+  {
+    author: "David Grann",
+    title: "The Wager: A Tale of Shipwreck, Mutiny and Murder",
+    img: img8,
+    id: 8,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "It Starts with Us",
+    img: img9,
+    id: 9,
+  },
+];
